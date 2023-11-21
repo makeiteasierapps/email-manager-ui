ad2089d5d93c45b53a1c5a438e6e2d744b81b8f5
Shaun Offenbacher <makeiteasierapps@gmail.com>
2023-11-21 14:54:46 -0700

    email templates

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,14 +1,73 @@
-// EmailForm.js
-import { Formik, Field, Form } from 'formik';
+import { useState, useEffect } from 'react';
+import { Formik, Field, Form, useFormik } from 'formik';
 import * as yup from 'yup';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { styled } from '@mui/system';
 import axios from 'axios';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const StyledField = styled(Field)(({ theme }) => ({
     marginBottom: theme.spacing(2),
 }));
+
+function createEmailTemplate(values) {
+    const templates = [
+        {
+            id: 1,
+            content: `
+                <h1> ${values.recipient_company} x Gal Media</h1>
+                <p>
+                    Hi ${values.recipient_name},
+                </p>
+                <p>
+                    Thanks for connecting with us on Instagram [or wherever you found them]. We love your brand and have been following your success!
+                </p>
+                <p>
+                    We've helped our clients achieve amazing results, such as getting them over 500+ press features, hosting Instagram-worthy events, TV Interviews, Podcasts and gaining millions of social impressions. <a href="your_deck_link_here">Here's a link</a> to our deck if you'd like to see some of our past work.
+                </p>
+                <p>
+                    We use a simple 4-step system to help our clients achieve their PR goals and rank higher on google, and I'd love to schedule a quick 20-minute call with you to discuss how we can implement this process to help take your brand to new heights.
+                </p>
+                <p>
+                    Are you available for a quick chat this week, or you can schedule a time on my calendar using this <a href="https://calendly.com/galmediagroup/consultation">link</a>.
+                </p>
+                <p>
+                    Looking forward to hearing from you!
+                </p>
+            `,
+        },
+        {
+            id: 2,
+            content: `
+                <h1>Press and Media Opportunities for ${values.recipient_company}</h1>
+                <p>
+                    Hi ${values.recipient_name},
+                </p>
+                <p>
+                    My name is ${values.sender_name}, I am the Senior Account Director at Gal Media. Our Founder, Jennifer Jaden, asked me to personally reach out to you and see who is the best contact on your team to discuss strategies for increasing your brand's exposure with press and media opportunities?
+                </p>
+                <p>
+                    We have a successful track record of assisting clients in securing 500+ press features, orchestrating Instagram-worthy events, TV Interviews, Podcasts and generating millions of social impressions for them. <a href="your_deck_link_here">Here's a link</a> to our deck showcasing some of our past work.
+                </p>
+                <p>
+                    Are you available for a quick chat this week? Or if you could direct me to the appropriate person on your team, that would be greatly appreciated.
+                </p>
+                <p>
+                    Thank you so much!
+                </p>
+            `,
+        },
+    ];
+    return templates;
+}
 
 const validationSchema = yup.object({
     sender_name: yup.string().required('Sender Name is required'),
@@ -32,6 +91,8 @@ const formFields = [
 ];
 
 export default function EmailForm() {
+    const [previewTemplate, setPreviewTemplate] = useState(null);
+    const [templates, setTemplates] = useState([]);
     const handleSubmit = async (values, actions) => {
         let data = {
             user_id: 'shauno.co',
@@ -63,50 +124,113 @@ export default function EmailForm() {
             );
         }
     };
+    
+    const formik = useFormik({
+        initialValues: {
+            useTemplate: false,
+            sender_name: '',
+            recipient_email: '',
+            recipient_name: '',
+            recipient_company: '',
+            subject: '',
+            message: '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: handleSubmit,
+    });
+
+    useEffect(() => {
+        const templates = createEmailTemplate(formik.values);
+        setTemplates(templates);
+    }, [formik.values]);
+
     return (
-        <Formik
-            initialValues={{
-                sender_name: '',
-                recipient_email: '',
-                recipient_name: '',
-                recipient_company: '',
-                subject: '',
-                message: '',
-            }}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-        >
-            {({ errors, touched, handleChange, values }) => (
-                <Form>
-                    {formFields.map((field) => (
-                        <StyledField
-                            key={field.id}
-                            as={TextField}
-                            fullWidth
-                            name={field.id}
-                            label={field.label}
-                            error={
-                                touched[field.id] && Boolean(errors[field.id])
-                            }
-                            helperText={touched[field.id] && errors[field.id]}
-                            {...(field.multiline && {
-                                multiline: true,
-                                rows: field.rows,
-                            })}
-                            onChange={handleChange}
-                            value={values[field.id]}
-                        />
-                    ))}
-                    <Button
-                        color="primary"
-                        variant="contained"
-                        fullWidth
-                        type="submit"
-                    >
-                        Submit
-                    </Button>
-                </Form>
-            )}
-        </Formik>
+        <>
+            <Formik {...formik}>
+                {({ errors, touched, handleChange, values }) => {
+                    return (
+                        <Form>
+                            <FormControlLabel
+                                control={
+                                    <Switch
+                                        checked={values.useTemplate}
+                                        onChange={handleChange}
+                                        name="useTemplate"
+                                        color="primary"
+                                    />
+                                }
+                                label="Use Template"
+                            />
+                            {formFields.map((field) =>
+                                field.id === 'message' && values.useTemplate ? (
+                                    <Carousel>
+                                        {templates.map((template) => (
+                                            <div
+                                                key={template.id}
+                                                onClick={() =>
+                                                    setPreviewTemplate(template)
+                                                }
+                                            >
+                                                <Card>
+                                                    <CardContent>
+                                                        <div
+                                                            dangerouslySetInnerHTML={{
+                                                                __html: template.content,
+                                                            }}
+                                                        />
+                                                    </CardContent>
+                                                </Card>
+                                            </div>
+                                        ))}
+                                    </Carousel>
+                                ) : (
+                                    <StyledField
+                                        key={field.id}
+                                        as={TextField}
+                                        fullWidth
+                                        name={field.id}
+                                        label={field.label}
+                                        error={
+                                            touched[field.id] &&
+                                            Boolean(errors[field.id])
+                                        }
+                                        helperText={
+                                            touched[field.id] &&
+                                            errors[field.id]
+                                        }
+                                        {...(field.multiline && {
+                                            multiline: true,
+                                            rows: field.rows,
+                                        })}
+                                        onChange={handleChange}
+                                        value={values[field.id]}
+                                    />
+                                )
+                            )}
+                            <Button
+                                color="primary"
+                                variant="contained"
+                                fullWidth
+                                type="submit"
+                            >
+                                Submit
+                            </Button>
+                        </Form>
+                    );
+                }}
+            </Formik>
+            <Dialog
+                open={!!previewTemplate}
+                onClose={() => setPreviewTemplate(null)}
+            >
+                <DialogContent>
+                    <div
+                        dangerouslySetInnerHTML={{
+                            __html: previewTemplate?.content,
+                        }}
+                    />
+                </DialogContent>
+            </Dialog>
+        </>
     );
 }
